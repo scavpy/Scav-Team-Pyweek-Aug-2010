ced539ab7c63a2d75c76dd1cd7a7818fed777a1a
scav <pete.alex.harris@gmail.com>
2010-08-22 13:07:06 +0100

    Added data/fonts directory, see if pyglet will load "IM FELL English Roman" from downloaded .ttf file. It seems not.

--- run_game.py
+++ run_game.py
@@ -1,4 +1,19 @@
 #! /usr/bin/env python
+import sys
+import os
+try:
+    here = os.path.abspath(os.path.dirname(__file__))
+    libdir = os.path.join(here, 'gamelib')
+    sys.path.insert(0, os.path.join(libdir,'tdgl.zip'))
+    sys.path.insert(0, libdir)
+    import pyglet.resource, pyglet.font
+    pyglet.resource.path=[".","data"]
+    pyglet.resource.reindex()
+    pyglet.font.add_directory(os.path.join(here,"data/fonts"))
+    assert pyglet.font.base.Font.have_font("IM FELL English Roman")
+except NameError:
+    # probably running inside py2exe which doesn't set __file__
+    pass
 
-from gamelib import main
+import main
 main.main()
