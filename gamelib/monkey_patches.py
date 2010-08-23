d92a6067dce23d5d3c7239448fa9d0572ec644b9
scav <pete.alex.harris@gmail.com>
2010-08-23 13:37:32 +0100

    Monkey-patch Panel.__del__ for case on program exit where global variables will be None

--- gamelib/monkey_patches.py
+++ gamelib/monkey_patches.py
@@ -3,14 +3,14 @@ As usual, I find bugs in tdgl...
 
 """
 
-from tdgl import lighting
-from tdgl.gl import glDisable, GL_LIGHT0
+import pyglet
+from tdgl import lighting, gl, panel
+from tdgl.gl import glDisable, GL_LIGHT0, glDeleteLists
 
 def fix_release_light(light):
     """Release a light so it can be used for other things"""
     if light in lighting.claimed_lights:
         lighting.light_switch(light,False)
-        print "glDisable(LIGHT0 +", light,")"
         glDisable(GL_LIGHT0 + light)
         lighting.claimed_lights.remove(light)
         lighting.free_lights.append(light)
@@ -18,3 +18,9 @@ def fix_release_light(light):
         del lighting.conditions[light]
 
 lighting.release_light = fix_release_light
+
+def fix_panel_del(self):
+    if glDeleteLists:
+        glDeleteLists(self.bgdl,2)
+
+panel.Panel.__del__ = fix_panel_del
